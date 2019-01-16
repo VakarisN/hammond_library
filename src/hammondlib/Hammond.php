00734ab98a29a14ecca68061448d358fd74f63ad
VakarisN <vakarisneverauskas@gmail.com>
2019-01-16 12:50:48 +0200

    Innitial Hammond setup src and test created

---
+++ src/hammondlib/Hammond.php
@@ -0,0 +1,8 @@
+<?php
+namespace Hammondlib;
+
+class Hammond {
+	public static function bacon_saldytuve() {
+		echo "Kiek šoninės telpa tavo šaldytuve: " . rand(15, 1000) . "kg";
+	}
+}
---
+++ tests/hammondlib/Hammond.php
@@ -0,0 +1,8 @@
+<?php
+namespace Hammondlib;
+
+class Hammond {
+	public static function bacon_saldytuve() {
+		echo "Testuojam kiek šoninės telpa tavo šaldytuve: " . rand(15, 1000) . "kg";
+	}
+}
