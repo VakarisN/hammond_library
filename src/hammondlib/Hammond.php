53ea3d8dcb8509059899d2222dc2b388b3719dac
VakarisN <vakarisneverauskas@gmail.com>
2019-01-17 11:49:26 +0200

    istaisyta funkcijos klaida su pavadinimu

--- src/hammondlib/Hammond.php
+++ src/hammondlib/Hammond.php
@@ -2,7 +2,7 @@
 namespace Hammondlib;
 
 class Hammond {
-	public static function bacon_saldytuve_padidintas_kiekis() {
+	public static function bacon_saldytuve() {
 		echo "Kiek šoninės telpa tavo šaldytuve: " . rand(400, 5000) . "kg";
 	}
 }
