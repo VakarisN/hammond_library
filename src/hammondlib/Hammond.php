976480b9cb57e44f5a04d7d8942550f9f06ee0f8
VakarisN <vakarisneverauskas@gmail.com>
2019-01-17 11:41:29 +0200

    Naujas release, padidintas kiekis saldytuve

--- src/hammondlib/Hammond.php
+++ src/hammondlib/Hammond.php
@@ -2,7 +2,7 @@
 namespace Hammondlib;
 
 class Hammond {
-	public static function bacon_saldytuve() {
-		echo "Kiek šoninės telpa tavo šaldytuve: " . rand(15, 1000) . "kg";
+	public static function bacon_saldytuve_padidintas_kiekis() {
+		echo "Kiek šoninės telpa tavo šaldytuve: " . rand(400, 5000) . "kg";
 	}
 }
